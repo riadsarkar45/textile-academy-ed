a5dc3e7cbbc8c7f91df26229d38099c8ebb34bf3
Riad Sarkar <riadsarkar45@gmail.com>
2026-02-02 19:50:36 +0600

    cors url's added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -39,9 +39,11 @@ app.register(fastifyJwt, {
   }
 })
 
+const allowedOrigins = ["https://textileacademy.vercel.app", "http://localhost:5173"]
+
 app.register(cors, {
-  origin: "http://localhost:5173",
-  credentials: true, 
+  origin: allowedOrigins,
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 });
