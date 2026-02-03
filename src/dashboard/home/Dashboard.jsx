9e41124383c7fb196661d4c1cd04fd3ee418b49b
Riad Sarkar <riadsarkar45@gmail.com>
2026-02-04 00:16:31 +0600

    subjectwise report is visible to student learning dashboard

--- src/dashboard/home/Dashboard.jsx
+++ src/dashboard/home/Dashboard.jsx
@@ -4,13 +4,15 @@ import useAxiosPublic from "../../hooks/Axios";
 const Dashboard = () => {
     const [studentSummary, setStudentSummary] = useState({})
     const [leaderBoard, setLeaderBoard] = useState([])
+    const [subjectWiseStats, setSubjectWiseStats] = useState([])
     const axiosPublic = useAxiosPublic();
     useEffect(() => {
         const fetchAttempts = async () => {
             try {
                 const res = await axiosPublic.get(`/summary`)
                 const leaderBoard = await axiosPublic.get(`/leaderboard`)
-                console.log(leaderBoard.data.leaderBoard);
+                console.log(res.data.subjectWiseStats, "subject wise stats");
+                setSubjectWiseStats(res.data.subjectWiseStats);
                 setLeaderBoard(leaderBoard?.data?.leaderboard);
                 setStudentSummary(res.data);
             } catch (err) {
@@ -19,10 +21,9 @@ const Dashboard = () => {
         }
         fetchAttempts();
     }, [axiosPublic])
-    console.log(studentSummary?.totalExamsAns?._sum?.correctAns, "checking");
-    console.log(studentSummary?.totalExamsAns?._sum?.wrongAns, "checking");
+
     return (
-        <div className="w-[50rem] m-auto">
+        <div className="w-[55rem] m-auto">
             <h2>Student Dashboard</h2>
             <div className="grid gap-2 grid-cols-2 mb-4">
                 <div className=" bg-gray-50 rounded-lg shadow-sm border-l-8 border-gray-500 h-[8rem]">
@@ -70,32 +71,43 @@ const Dashboard = () => {
                             )
                         })
                     }
-                    
+
                 </div>
                 <div className="">
                     <div className="bg-gray-50 shadow-sm text-gray-700 p-2 rounded-lg">
                         <h2 className="border-b p-3">Subject Wise Report</h2>
-                        <div className='flex border-b p-5 rounded-lg items-center mb-1'>
+                        {
+                            subjectWiseStats?.map((SUB_STATS, i) => {
+                                return (
+                                    <div key={i} className='flex border-b rounded-lg items-center mb-1'>
 
-                            <span className='ml-2'>Merchandising Introduction</span>
+                                        <div className="grid p-5">
 
-                            {/* push to right */}
-                            <span className='ml-auto'>#1</span>
-                        </div>
-                        <div className='flex border-b p-5 rounded-lg items-center mb-1'>
+                                            <span>{SUB_STATS?.subjectName}</span>
+                                            <div className="flex gap-2 text-sm">
+                                                <small className="text-green-700 border-green-400 border bg-green-500 inline-flex items-center bg-opacity-30 justify-center p-[1px] w-[4.4rem] text-md rounded-md">Attempts: {SUB_STATS?.attempts} </small>
+                                                <span className="inline-flex items-center justify-center h-[1.6rem] w-[6.4rem] rounded-[1.6rem] text-md bg-opacity-30 text-yellow-700 border-yellow-400 border bg-yellow-500">
+                                                    Questions:  {SUB_STATS?.totalQuestions}
+                                                </span>
+                                                <span className="inline-flex items-center justify-center h-[1.6rem] w-[5.1rem] rounded-[1.6rem] text-md bg-opacity-30 text-green-700 border-green-400 border bg-green-500">
+                                                    Correct: {SUB_STATS?.totalCorrect}
+                                                </span>
+                                                <span className="inline-flex items-center justify-center h-[1.6rem] w-[4.9rem] rounded-[1.6rem] text-md bg-opacity-30 text-red-700 border-red-400 border bg-red-500">
+                                                    Wrong: {SUB_STATS?.totalWrong}
+                                                </span>
 
-                            <span className='ml-2'>Planning Introduction</span>
+                                            </div>
 
-                            {/* push to right */}
-                            <span className='ml-auto'>#1</span>
-                        </div>
-                        <div className='flex border-b p-5 rounded-lg items-center mb-1'>
 
-                            <span className='ml-2'>T&A Introduction</span>
 
-                            {/* push to right */}
-                            <span className='ml-auto'>#1</span>
-                        </div>
+                                        </div>
+
+                                        {/* push to right */}
+                                    </div>
+                                )
+                            })
+                        }
+
                     </div>
                 </div>
             </div>
